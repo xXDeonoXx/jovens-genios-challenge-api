df67020c9ff72a2642626afc646f899b9a1eda67
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2022-09-25 01:28:19 -0300

    feat: create authentication resource and it's strategies

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Role } from 'src/enums/role.enum';
+import { Roles } from 'src/roles/roles.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { Public } from 'src/utils/Public';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+
+@Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Get('')
+  async testnoRole() {
+    return 'Alive, no roles';
+  }
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async authenticate(@Req() req): Promise<any> {
+    return this.authService.login(req.user);
+  }
+}
